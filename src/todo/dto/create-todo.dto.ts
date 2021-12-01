97dae5f889bc386eb9a1238c96ba63263a2c60da
Wouter127 <r0785520@student.thomasmore.be>
2021-12-01 15:18:29 +0100

    extra import

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,3 +1,6 @@
 import { BaseTodoDto } from "./base-todo.dto";
+import { ApiProperty, ApiBody} from '@nestjs/swagger';
 
-export class CreateTodoDto extends BaseTodoDto {}
+export class CreateTodoDto extends BaseTodoDto {
+    
+}
