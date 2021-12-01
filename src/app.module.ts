4b43bcb7f252d89b2c68b86b9e057ba0e1f1e822
Wouter127 <r0785520@student.thomasmore.be>
2021-12-01 14:59:03 +0100

    CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, MongooseModule.forRoot('mongodb+srv://nestj-workshop2:admin123@cluster0.nnflr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
